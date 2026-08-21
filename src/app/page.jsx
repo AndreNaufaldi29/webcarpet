a36378215fb062996443ecd090b8a044bfae4268
AndreNaufali29 <andrenaufaldi29@gmail.com>
2026-08-21 09:35:02 +0700

    update ui

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,23 @@
 "use client";
 
 import HeroSearch from "../components/HeroSearch";
-import Testimonial from "../components/Testimonial";
-import TestimonialForm from "../components/TestimonialForm";
 import Category from "../components/Category";
 import FeaturedCollection from "../components/FeaturedCollection";
 import LatestArrival from "../components/LatestArrival";
+import Testimonial from "../components/Testimonial";
+import TestimonialForm from "../components/TestimonialForm";
 
 export default function HomePage() {
   return (
     <>
       <HeroSearch />
-      <Testimonial />
-      <TestimonialForm />
       <Category />
       <FeaturedCollection />
       <LatestArrival />
+      <Testimonial />
+      <TestimonialForm />
     </>
   );
 }
+
 
